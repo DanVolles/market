8fa502d7b26f41e51ab5fe10fc7607c7cd30b9c6
Danilo <danilo.volles@outlook.com>
2022-11-14 18:30:03 -0300

    Project creation. Entity class creation. Setting pom.xml.

---
+++ src/main/java/senaijavadev/market/Product.java
@@ -0,0 +1,53 @@
+package senaijavadev.market;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+// Variable declaration
+        @Id
+        @GeneratedValue (strategy = GenerationType.AUTO)
+        private Integer id;
+        private String productName;
+        private String description;
+        private Double value;
+
+
+// Id
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+// productName
+    public String getProductName() {
+        return productName;
+    }
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+// Description
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+// Value
+    public Double getValue() {
+        return value;
+    }
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+}
